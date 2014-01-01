6d1839345b832cc7827bb9e8bf039c4943b9043b
Kevin Zhang <zhangkev@gmail.com>
2013-12-31 17:50:22 -0800

    Fixed slight issue with Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,5 +80,5 @@ Pinteresting::Application.configure do
 
   # Required for Heroku
   # Note to set this to your actual host 
-  config.action_mailer.default_url_options = ( :host => 'www.kevzhang.com')
+  config.action_mailer.default_url_options = { :host => 'www.kevzhang.com'}
 end
